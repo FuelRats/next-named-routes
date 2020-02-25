429cb3e8b7f391ab22e3fdceb2b3ac348733cec5
Cameron Welter <camjwelt@gmail.com>
2020-02-25 04:37:06 +0000

    use eslint override to set jest env on .test.js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,17 @@ module.exports = {
     '@fuelrats/eslint-config',
     '@fuelrats/eslint-config-react',
   ],
+  reportUnusedDisableDirectives: true,
   rules: {
     'jsdoc/require-jsdoc': ['off'],
     'react/prop-types': ['off'],
   },
+  overrides: [
+    {
+      files: ['*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
--- tests/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: '../.eslintrc',
-  env: {
-    jest: true,
-  },
-}
